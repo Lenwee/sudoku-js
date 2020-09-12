6897a2e788c182fd94647e44bd9c532b3e2029dc
Lewis Hammond <lewishammond97@gmail.com>
2020-09-12 16:58:43 +0100

    Adds arrow key support and tidy up

--- script.js
+++ script.js
@@ -16,13 +16,84 @@ $(function() {
   /*
     Helper functions
   */
-  function getHighlightedSudokuCells() {
+  function getSelectedSudokuCells() {
     return sudoku_cells.filter(function() {
-      if ($(this).find(".default").length === 1) {
-        return false;
-      }
       return $(this).hasClass("highlight");
     });
+  }
+
+  function addPencilCentered(cell, number) {
+    if (cell.find(".default").length !== 1) {
+      if (cell.find('[data-sudoku-value='+number+']').length === 0) {
+        if (cell.find('.centered').length > 0) {
+          cell.empty();
+        }
+        if (cell.find('span').length === 0) {
+          cell.append("<span class='pencil pencil--centered' data-sudoku-value='" + number + "'>" + number + "</span>");
+        } else {
+          var spanAdded = false;
+          $.each(cell.find('span'), function () {
+            if ($(this).data('sudoku-value') > number) {
+              spanAdded = true
+              cell.insertBefore("<span class='pencil pencil--centered' data-sudoku-value='" + number + "'>" + number + "</span>", $(this))
+            }
+          })
+          if (!spanAdded) {
+            cell.append("<span class='pencil pencil--centered' data-sudoku-value='" + number + "'>" + number + "</span>");
+          }
+        }
+      }
+    }
+  }
+
+  function removePencilCentered(cell, number) {
+    cell.find('[data-sudoku-value="'+number+'"]').remove();
+  }
+
+  function addNumber(cell, number) {
+    if (cell.find(".default").length !== 1) {
+      cell.empty();
+      cell.append("<span class='centered'>" + number + "</span>");
+    }
+  }
+
+  function moveSelected(cell, direction) {
+    var cellIndex = $(cell).index('.sudoku-cell'),
+      row = Math.floor(cellIndex/9),
+      col = cellIndex%9;
+
+    $.each(sudoku_cells, function() {
+      $(this).removeClass("highlight");
+    });
+
+    switch (direction) {
+      case 'ArrowUp':
+        row = row - 1
+        if (row < 0) {
+          row = 8;
+        }
+        break;
+      case 'ArrowRight':
+        col = col + 1
+        if (col >= 9) {
+          col = 0;
+        }
+        break;
+      case 'ArrowLeft':
+        col = col - 1
+        if (col < 0) {
+          col = 8;
+        }
+        break;
+      case 'ArrowDown':
+        row = row + 1
+        if (row >= 9) {
+          row = 0;
+        }
+        break;
+    }
+    var newCellIndex = (row * 9) + col;
+    $(sudoku_cells[newCellIndex]).addClass("highlight");
   }
 
   /*
@@ -74,28 +145,47 @@ $(function() {
   Number pressed on keyboard add value to selected
   */
   $(document).on("keydown", function(event) {
-    if (event.key > 0) {
-      var highlighted_cells = getHighlightedSudokuCells(),
-        user_number = event.key;
-      if (highlighted_cells.length > 1) {
-        $.each(highlighted_cells, function() {
-          var sudoku_cell = $(this);
-          sudoku_cell.empty();
-          sudoku_cell.append("<span class='pencil pencil--centered'>" + user_number + "</span>");
-        });
-      } else if (highlighted_cells.length === 1) {
-        $.each(highlighted_cells, function() {
-          var sudoku_cell = $(this);
-          sudoku_cell.empty();
-          sudoku_cell.append("<span class='centered'>" + user_number + "</span>");
-        });
+    var isShift = event.shiftKey,
+      keyPressed = event.key,
+      keyCodePressed = event.code,
+      selectedCells = getSelectedSudokuCells();
+
+    if (!isShift && keyPressed > 0) {
+      if (selectedCells.length > 1) {
+        var existingNotes = selectedCells.find('[data-sudoku-value='+keyPressed+']').length;
+        $.each(selectedCells, function () {
+          if (existingNotes < selectedCells.length) {
+            addPencilCentered($(this), keyPressed);
+          } else {
+            removePencilCentered($(this), keyPressed);
+          }
+        })
+      } else {
+        $.each(selectedCells, function () {
+          addNumber($(this), keyPressed);
+        })
       }
-    } else if (event.key === "Delete" || event.key === "Backspace") {
-      var highlighted_cells = getHighlightedSudokuCells();
-      $.each(highlighted_cells, function() {
-        var sudoku_cell = $(this);
-        sudoku_cell.empty();
+    }
+    else if (isShift && keyCodePressed.startsWith('Digit')){
+      var numberPressed = keyCodePressed.substr(keyCodePressed.length - 1)
+      if (numberPressed > 0) {
+        var existingNotes = selectedCells.find('[data-sudoku-value='+numberPressed+']').length;
+        $.each(selectedCells, function () {
+          if (existingNotes < selectedCells.length) {
+            addPencilCentered($(this), numberPressed);
+          } else {
+            removePencilCentered($(this), numberPressed);
+          }
+        })
+      }
+    }
+    else if (keyPressed === "Delete" || keyPressed === "Backspace") {
+      $.each(selectedCells, function() {
+        $(this).empty();
       });
+    }
+    else if (keyPressed.startsWith('Arrow')) {
+      moveSelected(selectedCells[0], keyPressed);
     }
   });
 });
